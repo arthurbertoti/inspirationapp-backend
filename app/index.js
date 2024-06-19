0d51b233a599b52189426552f9a91ac48a1005f0
Arthur Bertoti <artgbertoti@gmail.com>
2024-06-19 15:51:28 -0300

    feat(aws-sdk): add package use it for the environment variables

--- app/index.js
+++ app/index.js
@@ -2,12 +2,16 @@ const axios = require('axios');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const aws = require('aws-sdk');
 
-const appId = '10f1673c-d0e7-4aa4-a59a-89fe6d6c47d9';
+let s3 = new aws.S3({
+  appId: process.env.ONESIGNAL_API_KEY,
+  restApiKey: process.env.ONESIGNAL_REST_API_KEY
+});
 
-const restApiKey = 'ZTdlNzc5NzEtMzZlNC00ZGZhLWI4ZWUtMjNmNzA4ZTYwYWRk';
 app.get('/', (req, res) => {
   res.send('Server is running!');
+  console.log(s3)
 });
 
 app.listen(port, () => {
@@ -15,8 +19,9 @@ app.listen(port, () => {
 });
 
 app.get('/send-notification', async (req, res) => {
+  console.log(s3)
   const notification = {
-    app_id: appId,
+    app_id: s3.appId,
     included_segments: ['has_evening_advice'],
     headings: { en: 'Daily Advice' },
     contents: { en: 'Here is your daily advice! by app/index.js' }
@@ -26,7 +31,7 @@ app.get('/send-notification', async (req, res) => {
     const response = await axios.post('https://onesignal.com/api/v1/notifications', notification, {
       headers: {
         'Content-Type': 'application/json; charset=utf-8',
-        Authorization: `Basic ${restApiKey}`
+        Authorization: `Basic ${s3.restApiKey}`
       }
     });
     res.send('Notification sent: ' + JSON.stringify(response.data));
